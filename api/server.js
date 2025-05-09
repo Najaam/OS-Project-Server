9565ad29a7b13290ba8751a1f759b7288e016360
Najm-Ur-Rehman <67186639+Najaam@users.noreply.github.com>
2025-05-09 00:07:13 -0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const authroute = require("../Routes/Authroutes");
 const fs = require("fs");
+const db = require("../Db")
 
 const app = express();
 const PORT = 3000;
